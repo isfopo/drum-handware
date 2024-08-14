083ceaec4ae52b75db90133b9906732d9534b984
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-08-13 21:14:33 -0400

    ✨ (kick-riser-metal-base.ts): Create a new file to define a model for a metal base with a kick riser using JSCAD   📦 (kick-riser-metal-base.ts): Define a metal base with a kick riser, including dimensions and angles for extrusions and booleans   ♻️ (kick-riser-metal-base.ts): Refactor code to use variables for dimensions and angles to improve readability and maintainability

---
+++ src/kick-riser-metal-base.ts
@@ -0,0 +1,36 @@
+import { subtract } from "@jscad/modeling/src/operations/booleans";
+import { extrudeLinear } from "@jscad/modeling/src/operations/extrusions";
+import { cylinder, triangle } from "@jscad/modeling/src/primitives";
+import convert from "convert";
+
+const radius = convert(18, "in").to("mm");
+
+const segments = 100;
+
+const base = {
+  width: 150,
+  height: 50,
+  depth: 50,
+  angle: Math.PI / 2.5,
+};
+
+const slot = {
+  hole: 20,
+  width: 20,
+  height: 5,
+};
+
+export const main = () => {
+  return subtract(
+    extrudeLinear(
+      { height: base.depth },
+      triangle({ type: "ASA", values: [base.angle, base.width, base.angle] })
+    ),
+    cylinder({
+      radius: radius,
+      height: base.depth,
+      center: [base.width / 2, radius + base.height, base.depth / 2],
+      segments,
+    })
+  );
+};

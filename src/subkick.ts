37df96e5ac6de4b32b843e9785cc3e97eaab4aee
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-08-14 07:56:05 -0400

    ✨ (subkick.ts): introduce new geometries and operations to enhance the functionality of the subkick component in the CAD model. ♻️ (subkick.ts): refactor the screw generation logic into a separate function for better readability and maintainability.

--- src/subkick.ts
+++ src/subkick.ts
@@ -1,8 +1,11 @@
-import { subtract } from "@jscad/modeling/src/operations/booleans";
+import { Geom3 } from "@jscad/modeling/src/geometries/types";
+import { TAU } from "@jscad/modeling/src/maths/constants";
+import { subtract, union } from "@jscad/modeling/src/operations/booleans";
+import { rotate } from "@jscad/modeling/src/operations/transforms";
 import { cylinder } from "@jscad/modeling/src/primitives";
 import convert from "convert";
 
-const segments = 10;
+const segments = 50;
 
 const diameter = convert(8, "in").to("mm");
 const thickness = convert(0.5, "in").to("mm");
@@ -18,20 +21,43 @@ const rim = {
 };
 
 export const main = () => {
+  const screwGeo = () => {
+    const screws: Geom3[] = [];
+    for (let i = 0; i < rim.screws.count; i++) {
+      screws.push(
+        rotate(
+          [0, 0, i * (TAU / rim.screws.count)],
+          cylinder({
+            radius: rim.screws.diameter / 2,
+            height: rim.thickness * 2,
+            center: [
+              (diameter - rim.inset / 2) / 2,
+              0,
+              -height / 2 + rim.thickness,
+            ],
+            segments,
+          })
+        )
+      );
+    }
+    return screws;
+  };
   return subtract(
     cylinder({
       radius: diameter / 2 + thickness,
       height: height,
       segments,
     }),
+    ...screwGeo(),
+
     cylinder({
       radius: diameter / 2,
-      height: height - rim.thickness,
+      height: height - rim.thickness / 2,
       center: [0, 0, rim.thickness / 2],
       segments,
     }),
     cylinder({
-      radius: (diameter - rim.inset * 2) / 2,
+      radius: (diameter - rim.inset) / 2,
       height: height,
       segments,
     })

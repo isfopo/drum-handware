f7104628f4b190e7f3df0caef5e4462da96d95d7
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-08-15 09:21:46 -0400

    add flange mount

--- src/subkick.ts
+++ src/subkick.ts
@@ -1,15 +1,15 @@
 import { Geom3 } from "@jscad/modeling/src/geometries/types";
 import { TAU } from "@jscad/modeling/src/maths/constants";
 import { subtract, union } from "@jscad/modeling/src/operations/booleans";
-import { rotate } from "@jscad/modeling/src/operations/transforms";
+import { rotate, translate } from "@jscad/modeling/src/operations/transforms";
 import { cylinder } from "@jscad/modeling/src/primitives";
 import convert from "convert";
 
-const segments = 50;
+const segments = 100;
 
 const shell = {
   diameter: convert(8, "in").to("mm"),
-  thickness: convert(1 / 2, "in").to("mm"),
+  thickness: convert(1 / 4, "in").to("mm"),
   height: convert(4, "in").to("mm"),
 };
 
@@ -19,6 +19,18 @@ const rim = {
   screws: {
     diameter: convert(1 / 8, "in").to("mm"),
     count: 4,
+  },
+};
+
+const flangeMount = {
+  offset: -shell.height / 8,
+  angle: Math.PI / 4,
+  diameter: convert(1, "in").to("mm"),
+  thickness: convert(1 / 4, "in").to("mm"),
+  screws: {
+    count: 3,
+    diameter: convert(1 / 8, "in").to("mm"),
+    inset: convert(3 / 4, "in").to("mm"),
   },
 };
 
@@ -44,25 +56,87 @@ export const main = () => {
     }
     return screws;
   };
+  const transform = (geo: Geom3) =>
+    translate(
+      [(shell.diameter + shell.thickness) / 2, 0, flangeMount.offset],
+      rotate([0, Math.PI / 2, 0], geo)
+    );
 
-  return subtract(
-    cylinder({
-      radius: shell.diameter / 2 + shell.thickness,
-      height: shell.height,
-      segments,
-    }),
-    ...screwGeo(),
+  const flangeMountGeo = (isInner: boolean = false) => {
+    const flangeScrewGeo = () => {
+      const screws: Geom3[] = [];
+      for (let i = 0; i < flangeMount.screws.count; i++) {
+        screws.push(
+          transform(
+            rotate(
+              [0, 0, i * (TAU / flangeMount.screws.count)],
+              cylinder({
+                radius: flangeMount.screws.diameter / 2,
+                height: flangeMount.thickness,
+                center: [flangeMount.screws.inset / 2, 0, 0],
+                segments,
+              })
+            )
+          )
+        );
+      }
+      return screws;
+    };
 
-    cylinder({
-      radius: shell.diameter / 2,
-      height: shell.height - rim.thickness,
-      center: [0, 0, rim.thickness / 2],
-      segments,
-    }),
-    cylinder({
-      radius: (shell.diameter - rim.inset) / 2,
-      height: shell.height,
-      segments,
-    })
+    return {
+      outer: rotate(
+        [0, 0, flangeMount.angle],
+        transform(
+          cylinder({
+            radius: flangeMount.diameter / 2,
+            height: flangeMount.thickness,
+            center: [0, 0, 0],
+            segments,
+          })
+        )
+      ),
+      inner: rotate(
+        [0, 0, flangeMount.angle],
+        translate(
+          [-shell.thickness / 2, 0, 0],
+          transform(
+            cylinder({
+              radius: flangeMount.diameter / 2,
+              height: flangeMount.thickness,
+              center: [0, 0, 0],
+              segments,
+            })
+          )
+        )
+      ),
+      screws: rotate([0, 0, flangeMount.angle], flangeScrewGeo()),
+    };
+  };
+
+  return union(
+    subtract(
+      cylinder({
+        radius: shell.diameter / 2 + shell.thickness,
+        height: shell.height,
+        segments,
+      }),
+
+      ...screwGeo(),
+
+      cylinder({
+        radius: shell.diameter / 2,
+        height: shell.height - rim.thickness,
+        center: [0, 0, rim.thickness / 2],
+        segments,
+      }),
+      cylinder({
+        radius: (shell.diameter - rim.inset) / 2,
+        height: shell.height,
+        segments,
+      }),
+      flangeMountGeo().screws,
+      flangeMountGeo().inner
+    ),
+    subtract(flangeMountGeo().outer, flangeMountGeo().screws)
   );
 };

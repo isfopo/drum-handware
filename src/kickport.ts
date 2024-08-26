3f2ebf5c366680a47475bd04218e9df30f9eb685
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-08-25 23:54:19 -0400

    📝 (kickport.ts): Update kickport.ts to include a new Port part with its own geometric properties and calculations   ♻️ (kickport.ts): Refactor kickport.ts to use the updated Part enum and improve code organization and readability

--- src/kickport.ts
+++ src/kickport.ts
@@ -1,33 +1,92 @@
-import { subtract } from "@jscad/modeling/src/operations/booleans";
-import { cylinder } from "@jscad/modeling/src/primitives";
+import { subtract, union } from "@jscad/modeling/src/operations/booleans";
+import {
+  extrudeLinear,
+  extrudeRotate,
+} from "@jscad/modeling/src/operations/extrusions";
+import { arc, cylinder, line } from "@jscad/modeling/src/primitives";
+import { expand } from "@jscad/modeling/src/operations/expansions";
 import convert from "convert";
+import { Geom2, Path2 } from "@jscad/modeling/src/geometries/types";
 
 const segments = 50;
 
 enum Part {
   Ring,
+  Port,
 }
 
-const parts = Part.Ring as Part;
+const parts = Part.Port as Part;
 
-interface TemplateRingParams {
-  radius: number;
+const diameter = convert(5, "in").to("mm");
+
+interface RingParams {
+  diameter: number;
   thickness: number;
   width: number;
 }
 
-const ringGeo = ({ radius, thickness, width }: TemplateRingParams) => {
+interface PortParams {
+  diameter: number;
+  depth: number;
+  thickness: number;
+  outwardRadius: number;
+  rimWidth: number;
+}
+
+const ringGeo = ({ diameter, thickness, width }: RingParams) => {
   return subtract(
     cylinder({
       height: thickness,
-      radius: radius + width,
+      radius: diameter / 2 + width,
       segments,
     }),
     cylinder({
       height: thickness,
-      radius,
+      radius: diameter / 2,
       segments,
     })
+  );
+};
+
+const portGeo = ({
+  diameter,
+  depth,
+  thickness,
+  outwardRadius,
+  rimWidth,
+}: PortParams) => {
+  const innerDiameter = diameter - thickness - outwardRadius;
+
+  const round = (path: Path2) => {
+    return extrudeRotate(
+      { segments },
+      expand({ delta: thickness / 2, corners: "round" }, path)
+    );
+  };
+
+  return union(
+    round(
+      line([
+        [innerDiameter / 2, 0],
+        [innerDiameter / 2, depth],
+      ])
+    ),
+    round(
+      arc({
+        center: [(innerDiameter + outwardRadius) / 2 + thickness, depth],
+        radius: outwardRadius,
+        startAngle: Math.PI / 2,
+        endAngle: Math.PI,
+        segments,
+        makeTangent: true,
+      })
+    ),
+    round(
+      line([
+        [diameter / 2, depth + outwardRadius],
+        [diameter / 2 + rimWidth, depth + outwardRadius],
+      ])
+    )
   );
 };
 
@@ -35,9 +94,17 @@ export const main = () => {
   switch (parts) {
     case Part.Ring:
       return ringGeo({
-        radius: convert(5, "in").to("mm") / 2,
+        diameter,
         thickness: convert(1 / 8, "in").to("mm"),
         width: convert(1 / 2, "in").to("mm"),
+      });
+    case Part.Port:
+      return portGeo({
+        diameter,
+        depth: convert(6, "in").to("mm"),
+        thickness: convert(1 / 8, "in").to("mm"),
+        outwardRadius: convert(1 / 4, "in").to("mm"),
+        rimWidth: convert(1 / 4, "in").to("mm"),
       });
   }
 };
